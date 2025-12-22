9e4333a758b06b1452cb94dd32734033d706669c
Tjoostenm <topairy@gmail.com>
2025-12-22 19:22:11 +0100

    WIP

--- app/Filament/Clusters/UserManagement/Resources/UserResource/RelationManagers/AdminActionsRelationManager.php
+++ app/Filament/Clusters/UserManagement/Resources/UserResource/RelationManagers/AdminActionsRelationManager.php
@@ -4,11 +4,20 @@ declare(strict_types=1);
 
 namespace App\Filament\Clusters\UserManagement\Resources\UserResource\RelationManagers;
 
+use App\Features\DocumentationButtons;
+use App\Filament\Resources\Users\Pages\ViewUser;
+use App\Models\AdminActionLog;
 use BackedEnum;
+use Filament\Actions\Action;
+use Filament\Actions\ViewAction;
 use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Schemas\Components\Fieldset;
+use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Pennant\Feature;
 
 final class AdminActionsRelationManager extends RelationManager
 {
@@ -27,12 +36,51 @@ final class AdminActionsRelationManager extends RelationManager
             ->emptyStateHeading('Geen handelingen gevonden')
             ->emptyStateDescription('Er zijn momenteel geen gelogde handelingen gevonden die betrekking hebben tot dit gebruikersprofiel.')
             ->headerActions(actions: $this->registerHeaderActions())
+            ->recordActions(actions: $this->registerRecordActions())
             ->columns(components: $this->registerTableColumns());
+    }
+
+    public static function canViewForRecord(Model $ownerRecord, string $pageClass): bool
+    {
+        return new $pageClass() instanceof ViewUser;
+    }
+
+    public function infolist(Schema $schema): Schema
+    {
+        return $schema->components(components: [
+            Fieldset::make('Handeling')
+                ->columns(12)
+                ->columnSpanFull()
+                ->schema(components: []),
+
+            Fieldset::make('Uitgevoerd door')
+                ->columns(12)
+                ->columnSpanFull()
+                ->schema(components: []),
+        ]);
     }
 
     private function registerHeaderActions(): array 
     {
-        return [];
+        return [
+            Action::make('Help')
+                ->icon(Heroicon::OutlinedLifebuoy)
+                ->color('primary')
+                ->visible(Feature::active(DocumentationButtons::class))
+                ->url('https://www.google.com', shouldOpenInNewTab: true)
+        ];
+    }
+
+    private function registerRecordActions(): array 
+    {
+        return [
+            ViewAction::make()
+                ->modalHeading(fn (AdminActionLog $adminActionLog): string => "#{$adminActionLog->id} - {$adminActionLog->action}")
+                ->modalIcon(Heroicon::OutlinedInformationCircle)
+                ->modalIconColor('primary')
+                ->modalCloseButton(false)
+                ->modalDescription('Informatie omtrent een handeling die een administor heeft uitgevoerd met betrekking op het account.')
+        ];
     }
 
     private function registerTableColumns(): array 
@@ -41,7 +89,24 @@ final class AdminActionsRelationManager extends RelationManager
             TextColumn::make('causer.name')
                 ->icon(Heroicon::OutlinedUserCircle)
                 ->iconColor('primary')
-                ->label('Uitgevoerd door')
+                ->label('Uitgevoerd door'), 
+            TextColumn::make('ip_address')
+                ->icon(Heroicon::OutlinedGlobeEuropeAfrica)
+                ->label('IP adres'),
+            TextColumn::make('action')
+                ->label('Handeling')
+                ->badge()
+                ->icon(Heroicon::OutlinedTag)
+                ->searchable()
+                ->sortable(), 
+            TextColumn::make('description')
+                ->label('Beschrijving')
+                ->searchable(),
+            TextColumn::make('created_at')
+                ->label('Uitgevoerd op')
+                ->date()
+                ->sinceTooltip()
+                ->sortable(),
         ];
     }
 }
